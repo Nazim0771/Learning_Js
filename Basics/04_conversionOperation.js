3ac48c1f382e407f7504a95a12918fa11f0ce8b5
Nazim0771 <nazimkhan1860@gmail.com>
2024-09-02 14:09:38 +0000

    add conversionOperation

---
+++ Basics/04_conversionOperation.js
@@ -0,0 +1,39 @@
+let mark = 40 
+// console.log(typeof mark);
+// console.log(typeof(mark));
+// let mark1 = '40' 
+// console.log(typeof mark1);
+// console.log(typeof(mark1));
+// let isNumberValue = Number(mark1)
+// console.log(typeof(isNumberValue));
+let newMarks = '22bscs' 
+newMarks = null;
+// newMarks = undefined;
+// newMarks = true;
+let markInNumber = Number(newMarks)
+console.log(typeof(markInNumber));
+console.log(markInNumber); // in this case we will get Nan as a output 
+// when we convert null value or  into number then we will get 0 Zero  as a output
+// when we convert undefined value or  into number then we will Nan  as a output
+// true => 1  false => 0
+// let isLoggedInValue = 1 
+let isLoggedInValue = 'Nazim'
+let booleanIsLoggenInval = Boolean(isLoggedInValue)
+console.log(booleanIsLoggenInval);
+let isLoggedInValue2 = ''
+let booleanIsLoggenIn = Boolean(isLoggedInValue2)
+console.log(booleanIsLoggenIn);
+// 1 => true ; 0 => false 
+// '' => false 
+// 'nazim' => true
+
+let myNumber = 22
+let stringNumber = String(myNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+
+
+
+
